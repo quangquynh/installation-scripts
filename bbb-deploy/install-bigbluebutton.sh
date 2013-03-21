33cf6323be95d5734cd69d2f82f459cd199acf48
mrquangquynh <mrquangquynh@gmai.com>
2013-03-22 00:04:26 +0700

    Update install-bigbluebutton.sh

--- bbb-deploy/install-bigbluebutton.sh
+++ bbb-deploy/install-bigbluebutton.sh
@@ -3,13 +3,23 @@
 chmod +x install-ruby.sh
 ./install-ruby.sh
 
+#----------------------BBB 0.8---------------------------------
+# Add the BigBlueButton key
+#wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
+
+# Add the BigBlueButton repository URL and ensure the multiverse is enabled
+#echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_08/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+#echo "deb http://us.archive.ubuntu.com/ubuntu/ lucid multiverse" | sudo tee -a /etc/apt/sources.list
+#-----------------------------------------------------------------
+#---------------------BBB 0.81 -------------------------------------
 # Add the BigBlueButton key
 wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
 
 # Add the BigBlueButton repository URL and ensure the multiverse is enabled
-echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_08/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_081/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
 echo "deb http://us.archive.ubuntu.com/ubuntu/ lucid multiverse" | sudo tee -a /etc/apt/sources.list
 
+#------------------------------------------------------------------------
 echo "Updating the Ubuntu package repository"
 sudo apt-get update > /dev/null
 sudo apt-get -y dist-upgrade
