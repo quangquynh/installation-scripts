3e2bd7fccc5c245fc4ddb8f02d41350d922b2e58
mrquangquynh <mrquangquynh@gmai.com>
2013-04-28 22:29:09 +0700

    Update install-libreoffice.sh

--- bbb-deploy/install-libreoffice.sh
+++ bbb-deploy/install-libreoffice.sh
@@ -2,7 +2,7 @@
 
 wget http://bigbluebutton.googlecode.com/files/openoffice.org_1.0.4_all.deb
 sudo dpkg -i openoffice.org_1.0.4_all.deb
-sudo apt-get install python-software-properties
+sudo apt-get install -y python-software-properties
 
 sudo apt-add-repository ppa:libreoffice/libreoffice-4-0
 sudo apt-get update
