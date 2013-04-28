4f2ef0e9c5a072c361a3a80842b5f4d0fa9b2900
mrquangquynh <mrquangquynh@gmai.com>
2013-04-28 11:26:02 +0700

    Create install-bigbluebutton0.80.sh

---
+++ bbb-deploy/install-bigbluebutton0.80.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#-----------------Install Ruby-------------------------------
+chmod +x install-ruby.sh
+./install-ruby.sh
+
+#-----------------Install LibreOffice------------------------- 
+#chmod +x install-libreoffice.sh
+#./install-libreoffice.sh
+
+#-----------------Install ffmpeg------------------------------
+#chmod +x install-ffmpeg.sh
+#./install-ffmpeg.sh
+
+#----------------------BBB 0.8---------------------------------
+# Add the BigBlueButton key
+wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
+
+# Add the BigBlueButton repository URL and ensure the multiverse is enabled
+echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_08/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ lucid multiverse" | sudo tee -a /etc/apt/sources.list
+#-----------------------------------------------------------------
+#---------------------BBB 0.81 -------------------------------------
+# Add the BigBlueButton key
+wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
+
+# Add the BigBlueButton repository URL and ensure the multiverse is enabled
+#echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_081/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+#echo "deb http://us.archive.ubuntu.com/ubuntu/ lucid multiverse" | sudo tee -a /etc/apt/sources.list
+
+#------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------
+echo "Updating the Ubuntu package repository"
+sudo apt-get update > /dev/null
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install bigbluebutton bbb-demo
